fixed vertex matching for similarity calculation

find_most_similar.py
@@ -1,46 +1,48 @@
-import sys    
+from load_data import load_all_timelines, load_timeline, load_graph
 import collections   
 import numpy as np
-from load_data import load_all_timelines, load_timeline, load_graph
 from n2vec import embed_graphs
 from numpy.linalg import norm
 
-sys.path.insert(1, '/Users/barkincavdaroglu/Desktop/Most-Similar-Graph-Embeddings/data_utils')     
 
 def find_most_similar_timeline(filenames):
-    timelines = load_all_timelines(filenames) # key: time, value: (adj_matrix, nx_graphs)
+    timelines = load_all_timelines(filenames) 
 
-    current_snapshot = load_timeline("sample_data/0.txt")
-    _, current_snapshot_graph = load_graph(current_snapshot)
-    current_snapshot_graph = current_snapshot_graph[0]
+    current_timeline = load_all_timelines(["sample_data/0.txt"])
+    current_timeline = current_timeline[0]
+    current_snapshot_graph = current_timeline.get_nx_graph_at_time(0)
     current_snapshot_model = embed_graphs(current_snapshot_graph)
 
     curr_min_euc, curr_min_pointer_euc = float('INF'), 0
-    curr_min_cos, curr_min_pointer_cos = float('INF'), 0
+    curr_max_cos, curr_max_pointer_cos = float('-INF'), 0
 
     model_embeds_of_timeline = collections.defaultdict(lambda: [])
 
-    for key, val in timelines.items(): # key is timeline id
-        for i, nx_graph in enumerate(val[1]):
+    for key, timeline in timelines.items(): # key is timeline id
+        for i in range(timeline.get_snapshot_count()):
             embedding_filename = "time_" + str(key) + "_" + str(i) + "_embedding"
             embedding_model_filename = "time_" + str(key) + "_" + str(i) + "_embedding_model"
             edge_embedding_filename = "time_" + str(key) + "_" + str(i) + "_edges_embedding"
-            model = embed_graphs(val[1][i], embedding_filename=embedding_filename, embedding_model_filename=embedding_model_filename, edges_embedding_filename=edge_embedding_filename)
+            model = embed_graphs(timeline.get_nx_graph_at_time(i), embedding_filename=embedding_filename, embedding_model_filename=embedding_model_filename, edges_embedding_filename=edge_embedding_filename)
             model_embeds_of_timeline[key].append(model)
         
-        for i in range(1, len(val[1])):
+        for i in range(1, timeline.get_snapshot_count()):
             curr_prev_snapshot = model_embeds_of_timeline[key][i]
             prev_prev_snapshot = model_embeds_of_timeline[key][i - 1]
 
             for node in current_snapshot_graph.nodes():
+                matched_order_in_other_timeline = timeline.get_matching_node(i, node)
+                
                 vec1 = current_snapshot_model.wv[node]
-                vec2 = curr_prev_snapshot.wv[node]
+                vec2 = curr_prev_snapshot.wv[matched_order_in_other_timeline]
                 dist1_euc = np.linalg.norm(vec1 - vec2)
-                dist1_cos = np.dot(vec1,vec2)/(norm(vec1) * norm(vec2))
+                dist1_cos = np.dot(vec1, vec2) / (norm(vec1) * norm(vec2))
 
-                vec3 = prev_prev_snapshot.wv[node]
+                matched_order_in_other_timeline = timeline.get_matching_node(i - 1, node)
+
+                vec3 = prev_prev_snapshot.wv[matched_order_in_other_timeline]
                 dist2_euc = np.linalg.norm(vec1 - vec3)
-                dist2_cos = np.dot(vec1,vec3)/(norm(vec1) * norm(vec3))
+                dist2_cos = np.dot(vec1, vec3)/(norm(vec1) * norm(vec3))
 
                 if dist1_euc + dist2_euc < curr_min_euc:
                     curr_min_euc = dist1_euc + dist2_euc
@@ -49,14 +51,16 @@ def find_most_similar_timeline(filenames):
                     else:
                         curr_min_pointer_euc = (key, i - 1)
 
-                if dist1_cos + dist2_cos < curr_min_cos:
-                    curr_min_cos = dist1_cos + dist2_cos
-                    if dist1_cos < dist2_cos:
-                        curr_min_pointer_cos = (key, i)
+                if dist1_cos + dist2_cos > curr_max_cos:
+                    curr_max_cos = dist1_cos + dist2_cos
+                    if dist1_cos > dist2_cos:
+                        curr_max_pointer_cos = (key, i)
                     else:
-                        curr_min_pointer_cos = (key, i - 1)
+                        curr_max_pointer_cos = (key, i - 1)
                         
-    return curr_min_pointer_euc, curr_min_pointer_cos
+    return curr_min_pointer_euc, curr_max_pointer_cos
+
+    
 
 filenames = ["sample_data/1.txt", "sample_data/2.txt"]
 
@@ -66,5 +70,3 @@ print(find_most_similar_timeline(filenames))
 print(find_most_similar_timeline(filenames))
 print(find_most_similar_timeline(filenames))
 print(find_most_similar_timeline(filenames))
-
-    

load_data.py
@@ -1,50 +1,60 @@
 import numpy as np
 import networkx as nx
 from karateclub import DeepWalk, Walklets
+from timeline import Timeline
 
 def load_all_timelines(filenames):
     timelines = {}
 
     for i, filename in enumerate(filenames):
-        timeline = load_timeline(filename)
-        graphs_of_timeline = load_graph(timeline)
-        timelines[i] = graphs_of_timeline
+        timeline = Timeline()
+        timeline_g = load_timeline(filename, timeline)
+        _ = load_graph(timeline_g, timeline)
+        timelines[i] = timeline
 
     return timelines
 
-def load_timeline(filename="sample_data/1.txt"):
+def load_timeline(filename, timeline):
     temp = open(filename,'r').read().splitlines()
-    graphs, curr_graph, node_to_order, size  = [], [], {}, 0
+    graphs, curr_graph, node_to_order, order_to_node, size  = [], [], {}, {}, 0
+    time = 0
 
     for line in temp:
         if len(line) > 1:
             line_split = line.split()
             if line_split[0] == "O:":
                 node_to_order[line_split[1]] = int(line_split[2])
+                order_to_node[int(line_split[2])] = line_split[1]
                 size += 1
             else:
                 v1, v2, weight = node_to_order[line_split[1]], node_to_order[line_split[2]], float(line_split[3])
                 curr_graph.append((v1, v2, weight))
 
         elif len(line) == 0: # another graph
+            timeline.add_ordering_at_time(node_to_order, order_to_node, time)
             curr_graph.append(size)
             graphs.append(curr_graph)
             curr_graph = []
+            node_to_order = {}
+            order_to_node = {}
+            time += 1
             size = 0
 
+    timeline.add_ordering_at_time(node_to_order, order_to_node, time)
     curr_graph.append(size)
     graphs.append(curr_graph)
 
     return graphs
 
-def load_graph(graphs):
-    nx_graphs = []
+def load_graph(graphs, timeline):
     adj_matrices = []
+    time = 0
 
     for graph in graphs:
         G = nx.Graph()
 
         graph_size = graph[-1]
+        timeline.add_size_at_time(time, graph_size)
         adj_matrix = np.zeros((graph_size, graph_size))
         
         for edge in graph[:-1]:
@@ -54,10 +64,16 @@ def load_graph(graphs):
             adj_matrix[v1][v2] = weight
             adj_matrix[v2][v1] = weight
         adj_matrices.append(adj_matrix)
-        nx_graphs.append(G)
+        timeline.add_snapshot_graph_at_time(time, G)
+        time += 1
 
-    return adj_matrices, nx_graphs
+    return adj_matrices
 
+"""filenames = ["sample_data/1.txt", "sample_data/2.txt"]
+timelines = load_all_timelines(filenames)
+for i in timelines.keys():
+    timelines[i].get_all()
+    print("\n")"""
 
 
 """model = DeepWalk()