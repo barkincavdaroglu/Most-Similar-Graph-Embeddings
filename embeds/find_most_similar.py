8da7c250941c2f716abaf93d88924494d739b549
BC <barkincavdar@gmail.com>
2022-11-07 10:08:28 -0500

    cleaned up v1

--- embeds/find_most_similar.py
+++ embeds/find_most_similar.py
@@ -1,16 +1,13 @@
-# We will take a snapshot of a graph and find the sequence of graphs most similar to it
-
-# importing the sys module
 import sys    
 import collections   
 import numpy as np
+from load_data import load_all_timelines, load_timeline, load_graph
+from n2vec import embed_graphs
+from numpy.linalg import norm
 
 sys.path.insert(1, '/Users/barkincavdaroglu/Desktop/Most-Similar-Graph-Embeddings/data_utils')     
 
-from load_data import load_all_timelines, load_timeline, load_graph
-from n2vec import embed_graphs
-
-def find_most_similar_timeline(filenames=["sample_data/1.txt", "sample_data/2.txt"]):
+def find_most_similar_timeline(filenames):
     timelines = load_all_timelines(filenames) # key: time, value: (adj_matrix, nx_graphs)
 
     current_snapshot = load_timeline("sample_data/0.txt")
@@ -18,7 +15,9 @@ def find_most_similar_timeline(filenames=["sample_data/1.txt", "sample_data/2.tx
     current_snapshot_graph = current_snapshot_graph[0]
     current_snapshot_model = embed_graphs(current_snapshot_graph)
 
-    curr_min, curr_min_pointer = float('INF'), 0
+    curr_min_euc, curr_min_pointer_euc = float('INF'), 0
+    curr_min_cos, curr_min_pointer_cos = float('INF'), 0
+
     model_embeds_of_timeline = collections.defaultdict(lambda: [])
 
     for key, val in timelines.items(): # key is timeline id
@@ -36,20 +35,36 @@ def find_most_similar_timeline(filenames=["sample_data/1.txt", "sample_data/2.tx
             for node in current_snapshot_graph.nodes():
                 vec1 = current_snapshot_model.wv[node]
                 vec2 = curr_prev_snapshot.wv[node]
-                dist1 = np.linalg.norm(vec1 - vec2)
+                dist1_euc = np.linalg.norm(vec1 - vec2)
+                dist1_cos = np.dot(vec1,vec2)/(norm(vec1) * norm(vec2))
 
                 vec3 = prev_prev_snapshot.wv[node]
-                dist2 = np.linalg.norm(vec1 - vec3)
-                if dist1 + dist2 < curr_min:
-                    curr_min = dist1 + dist2
-                    if dist1 < dist2:
-                        curr_min_pointer = (key, i)
+                dist2_euc = np.linalg.norm(vec1 - vec3)
+                dist2_cos = np.dot(vec1,vec3)/(norm(vec1) * norm(vec3))
+
+                if dist1_euc + dist2_euc < curr_min_euc:
+                    curr_min_euc = dist1_euc + dist2_euc
+                    if dist1_euc < dist2_euc:
+                        curr_min_pointer_euc = (key, i)
                     else:
-                        curr_min_pointer = (key, i - 1)
+                        curr_min_pointer_euc = (key, i - 1)
 
-    return curr_min_pointer
+                if dist1_cos + dist2_cos < curr_min_cos:
+                    curr_min_cos = dist1_cos + dist2_cos
+                    if dist1_cos < dist2_cos:
+                        curr_min_pointer_cos = (key, i)
+                    else:
+                        curr_min_pointer_cos = (key, i - 1)
+                        
+    return curr_min_pointer_euc, curr_min_pointer_cos
 
-print(find_most_similar_timeline())
-    
+filenames = ["sample_data/1.txt", "sample_data/2.txt"]
+
+print(find_most_similar_timeline(filenames))
+print(find_most_similar_timeline(filenames))
+print(find_most_similar_timeline(filenames))
+print(find_most_similar_timeline(filenames))
+print(find_most_similar_timeline(filenames))
+print(find_most_similar_timeline(filenames))
 
     
--- embeds/n2vec.py
+++ embeds/n2vec.py
@@ -3,7 +3,7 @@ from node2vec import Node2Vec
 from numpy.random import seed
 
 # Accepts a networkx graph and returns a its embedding using Node2Vec
-def embed_graphs(graph, embedding_filename="", embedding_model_filename="", edges_embedding_filename="", embedding_dir="graph_embed_data/"):
+def embed_graphs(graph, embedding_filename="", embedding_model_filename="", edges_embedding_filename="", embedding_dir="graph_embed_data/", save_embed_files=False):
     EMBEDDING_FILENAME = embedding_dir + embedding_filename + ".emb" # "graph_embed_data/embeddings.emb"
     EMBEDDING_MODEL_FILENAME = embedding_dir + embedding_model_filename + ".model" # "graph_embed_data/embeddings.model"
     EDGES_EMBEDDING_FILENAME = embedding_dir + edges_embedding_filename + ".emb" # "graph_embed_data/edges.emb"
@@ -18,21 +18,26 @@ def embed_graphs(graph, embedding_filename="", embedding_model_filename="", edge
         window=10, min_count=1, batch_words=4
     )  # Any keywords acceptable by gensim.Word2Vec can be passed, `dimensions` and `workers` are automatically passed (from the Node2Vec constructor)
 
-    # Save embeddings for later use
-    #model.wv.save_word2vec_format(EMBEDDING_FILENAME)
-
-    # Save model for later use
-    #model.save(EMBEDDING_MODEL_FILENAME)
-
     # Embed edges using Hadamard method
     from node2vec.edges import HadamardEmbedder
 
     edges_embs = HadamardEmbedder(keyed_vectors=model.wv)
 
-    # Get all edges in a separate KeyedVectors instance - use with caution could be huge for big networks
-    #edges_kv = edges_embs.as_keyed_vectors()
-
-    # Save embeddings for later use
-    #edges_kv.save_word2vec_format(EDGES_EMBEDDING_FILENAME)
+    if save_embed_files:
+        save_embed_files(EMBEDDING_FILENAME, EDGES_EMBEDDING_FILENAME, EMBEDDING_MODEL_FILENAME, model, edges_embs)
 
     return model
+
+
+def save_embed_files(embed_filename, edge_embed_filename, model_embed_filename, nmodel, emodel):
+    # Save embeddings for later use
+    nmodel.wv.save_word2vec_format(embed_filename)
+
+    # Save model for later use
+    nmodel.save(model_embed_filename)
+
+    # Get all edges in a separate KeyedVectors instance - use with caution could be huge for big networks
+    edges_kv = emodel.as_keyed_vectors()
+
+    # Save embeddings for later use
+    edges_kv.save_word2vec_format(edge_embed_filename)
