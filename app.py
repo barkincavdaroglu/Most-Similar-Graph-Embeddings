f0932edfff06aea6e455edc075e004a0c1fb31f3
BC <barkincavdar@gmail.com>
2022-11-10 23:25:19 -0500

    updated function to find most similar to 2 graphs

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ from find_most_similar import find_most_similar_timeline
 
 app = Flask(__name__)
 
-@app.route('/get_next_state', methods=['POST'])
+
+@app.route("/get_next_state", methods=["POST"])
 def get_next_state():
     prev_timeline_filenames = ["sample_data/1.txt", "sample_data/2.txt"]
-    graph_snapshot = request.get_json()['snapshot']
-    most_similar_timeline, snapshot_key = find_most_similar_timeline(prev_timeline_filenames, graph_snapshot, "string")
+    graph_snapshots = request.get_json()["snapshots"]
+    most_similar_timeline, snapshot_key = find_most_similar_timeline(
+        prev_timeline_filenames, graph_snapshots, "string"
+    )
 
-    data = open(prev_timeline_filenames[most_similar_timeline],'r').read().splitlines()
+    data = open(prev_timeline_filenames[most_similar_timeline], "r").read().splitlines()
 
     time = 0
     graph_to_return = ""
@@ -19,9 +22,9 @@ def get_next_state():
         if len(line) >= 1 and time == snapshot_key + 1:
             graph_to_return += line + "\n"
 
-        elif len(line) == 0: # another graph
+        elif len(line) == 0:  # another graph
             if time == snapshot_key + 1:
-                return graph_to_return
+                return jsonify(snapshot=graph_to_return[:-1])
             time += 1
 
-    return jsonify(snapshot=graph_to_return)
+    return jsonify(snapshot=graph_to_return[:-1])
